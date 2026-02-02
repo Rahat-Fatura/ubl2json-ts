e3c0c7f5fb356a8a4f44d7bc96569f3b9a635380
Berkay Gökçe <berkaygkc7@gmail.com>
2026-02-02 17:09:42 +0300

    chore: bump version to 1.0.2 and export Invoice enum types

--- src/index.ts
+++ src/index.ts
@@ -6,106 +6,111 @@
  */
 
 // Main converter classes
-export { InvoiceConverter } from './converters/invoice-converter';
-export { DespatchConverter } from './converters/despatch-converter';
+export { InvoiceConverter } from "./converters/invoice-converter";
+export { DespatchConverter } from "./converters/despatch-converter";
 
 // Enums
-export { DespatchProfileId, DespatchTypeCode } from './types';
+export {
+    InvoiceProfileId,
+    InvoiceTypeCode,
+    DespatchProfileId,
+    DespatchTypeCode,
+} from "./types";
 
 // Error classes
-export { UblParseError } from './errors/ubl-parse-error';
+export { UblParseError } from "./errors/ubl-parse-error";
 
 // Configuration
-export { parserOptions } from './config/parser-config';
+export { parserOptions } from "./config/parser-config";
 
 // Types - All exported for library users
 export type {
-  // Raw XML types
-  XmlValue,
-  XmlAmount,
-  XmlQuantity,
-  XmlId,
-  RawPartyIdentification,
-  RawPerson,
-  RawPartyName,
-  RawTaxScheme,
-  RawPartyTaxScheme,
-  RawCountry,
-  RawPostalAddress,
-  RawContact,
-  RawPartyLegalEntity,
-  RawParty,
-  RawCustomerParty,
-  RawTaxCategory,
-  RawTaxSubtotal,
-  RawTaxTotal,
-  RawAllowanceCharge,
-  RawPrice,
-  RawGoodsItem,
-  RawShipment,
-  RawDelivery,
-  RawItemInstance,
-  RawItem,
-  RawInvoiceLine,
-  RawLegalMonetaryTotal,
-  RawPricingExchangeRate,
-  RawDespatchDocumentReference,
-  RawOrderReference,
-  RawFinancialAccount,
-  RawPaymentMeans,
-  RawAttachment,
-  RawAdditionalDocumentReference,
-  RawInvoice,
-  ParsedXmlRoot,
-  ParsedInvoiceXmlRoot,
-  // Raw Despatch types
-  RawDriverPerson,
-  RawRoadTransport,
-  RawTransportMeans,
-  RawShipmentStage,
-  RawCarrierParty,
-  RawDespatch,
-  RawDeliveryAddress,
-  RawShipmentDelivery,
-  RawDespatchShipment,
-  RawDespatchContact,
-  RawDespatchSupplierParty,
-  RawDeliveryCustomerParty,
-  RawOrderLineReference,
-  RawDespatchLine,
-  RawDespatchAdvice,
-  ParsedDespatchXmlRoot,
-  // Normalized output types
-  TaxSubtotal,
-  AllowanceCharge,
-  ItemInstance,
-  LineAdditional,
-  InvoiceLine,
-  AdditionalIdentifier,
-  Party,
-  DespatchReference,
-  OrderReference,
-  PaymentMeans,
-  AdditionalDocumentReference,
-  Invoice,
-  // Normalized Despatch types
-  DriverPerson,
-  CarrierParty,
-  DeliveryAddress,
-  ShipmentInfo,
-  DespatchLine,
-  DespatchAdvice,
-  // Config types
-  InvoiceConverterOptions,
-  ParserConfig,
-  UblParseErrorData,
-} from './types';
+    // Raw XML types
+    XmlValue,
+    XmlAmount,
+    XmlQuantity,
+    XmlId,
+    RawPartyIdentification,
+    RawPerson,
+    RawPartyName,
+    RawTaxScheme,
+    RawPartyTaxScheme,
+    RawCountry,
+    RawPostalAddress,
+    RawContact,
+    RawPartyLegalEntity,
+    RawParty,
+    RawCustomerParty,
+    RawTaxCategory,
+    RawTaxSubtotal,
+    RawTaxTotal,
+    RawAllowanceCharge,
+    RawPrice,
+    RawGoodsItem,
+    RawShipment,
+    RawDelivery,
+    RawItemInstance,
+    RawItem,
+    RawInvoiceLine,
+    RawLegalMonetaryTotal,
+    RawPricingExchangeRate,
+    RawDespatchDocumentReference,
+    RawOrderReference,
+    RawFinancialAccount,
+    RawPaymentMeans,
+    RawAttachment,
+    RawAdditionalDocumentReference,
+    RawInvoice,
+    ParsedXmlRoot,
+    ParsedInvoiceXmlRoot,
+    // Raw Despatch types
+    RawDriverPerson,
+    RawRoadTransport,
+    RawTransportMeans,
+    RawShipmentStage,
+    RawCarrierParty,
+    RawDespatch,
+    RawDeliveryAddress,
+    RawShipmentDelivery,
+    RawDespatchShipment,
+    RawDespatchContact,
+    RawDespatchSupplierParty,
+    RawDeliveryCustomerParty,
+    RawOrderLineReference,
+    RawDespatchLine,
+    RawDespatchAdvice,
+    ParsedDespatchXmlRoot,
+    // Normalized output types
+    TaxSubtotal,
+    AllowanceCharge,
+    ItemInstance,
+    LineAdditional,
+    InvoiceLine,
+    AdditionalIdentifier,
+    Party,
+    DespatchReference,
+    OrderReference,
+    PaymentMeans,
+    AdditionalDocumentReference,
+    Invoice,
+    // Normalized Despatch types
+    DriverPerson,
+    CarrierParty,
+    DeliveryAddress,
+    ShipmentInfo,
+    DespatchLine,
+    DespatchAdvice,
+    // Config types
+    InvoiceConverterOptions,
+    ParserConfig,
+    UblParseErrorData,
+} from "./types";
 
 // Utility functions (for advanced users)
 export {
-  setDefaults,
-  normalizeTaxSubtotals,
-  normalizeAllowanceCharges,
-  normalizeLines,
-  normalizeParty,
-} from './utils/normalizers';
+    setDefaults,
+    normalizeTaxSubtotals,
+    normalizeAllowanceCharges,
+    normalizeLines,
+    normalizeParty,
+} from "./utils/normalizers";
